d773929f8b7d30121c7c772c84fa5896f0eb6172
scrivts <4ntjdev+pro@gmail.com>
2024-10-20 18:34:54 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+--[[
+Scrivts
+--]]
+
+if game.PlaceId == 79974059203827 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/scrivts/Scrivts/refs/heads/main/79974059203827/source.lua"))()
+  return
+else
+  warn("No Scripts Found!")
+  return
+end
