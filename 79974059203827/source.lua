eb7e04876f264b7857b6526ab1da4a63ddef6888
scrivts <185070691+scrivts@users.noreply.github.com>
2024-10-31 19:23:41 +0100

    unpatched freaky prison line (tested only on synapse z)

--- 79974059203827/source.lua
+++ 79974059203827/source.lua
@@ -1,69 +1,69 @@
 if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Main"):FindFirstChild("ToiletChat"):FindFirstChild("TemplateButton"):FindFirstChild("MainLocalScript") then
-   local hash = getscripthash(game:GetService("Players").LocalPlayer.PlayerGui.Main.ToiletChat.TemplateButton.MainLocalScript)
-   if hash ~= "d80d093d39dcb8f0ff8fd379b21e87cab1bf19cf883b5f1dbd1485756abe7d7e516fb39289682953eecdf7a0392f4612" then
-       game:Shutdown()
-   end
- else
-   game:Shutdown()
- end
- 
- local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/scrivts/Rayfield/refs/heads/main/source.lua'))()
- 
- local args = {
-    [1] = 1,
-    [2] = 0,
-    [3] = "IWISHYOUGOODLUCK777888BESTGAMESLAY99COOLGAMECHANGEDO"
- }
- 
- local function addMoney(money)
-   local args2 = {
-    [1] = 1,
-    [2] = -money,
-    [3] = "IWISHYOUGOODLUCK777888BESTGAMESLAY99COOLGAMECHANGEDO"
-    }
- 
-    game:GetService("ReplicatedStorage"):WaitForChild("CutInLineEvent2"):FireServer(unpack(args2))
- 
-    local args3 = {
-      [1] = "IWISHYOUGOODLUCK777888BESTGAMESLAY99COOLGAMECHANGEDO"
+  local hash = getscripthash(game:GetService("Players").LocalPlayer.PlayerGui.Main.ToiletChat.TemplateButton.MainLocalScript)
+  if hash ~= "d41d8cd98f00b204e9800998ecf8427e" then
+      game:Shutdown()
+  end
+else
+  game:Shutdown()
+end
+
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/scrivts/Rayfield/refs/heads/main/source.lua'))()
+
+local args = {
+   [1] = 1,
+   [2] = 0,
+   [3] = "CLUBCLUBGOODLUCK777888BESTGAMEPLAY99COOLGAMECHANGEDO"
+}
+
+local function addMoney(money)
+  local args2 = {
+   [1] = 1,
+   [2] = -money,
+   [3] = "CLUBCLUBGOODLUCK777888BESTGAMEPLAY99COOLGAMECHANGEDO"
    }
-   game:GetService("ReplicatedStorage"):WaitForChild("PUSHMEBACK"):FireServer(unpack(args3))
- end
- 
- local Window = Rayfield:CreateWindow({
-   Name = "Scrivts",
-   LoadingTitle = "Freaky Prison Line Simulator",
-   LoadingSubtitle = "by Scrivts",
- 
-   KeySystem = false,
- 
-    KeySettings = {
-      Title = "Scrivts",
-      Subtitle = "Key System",
-      Note = "The key is Scrivts",
-      Key = {"Scrivts"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
- })
- 
- local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
- 
- local Cut = Tab:CreateButton({
-   Name = "Cut in line",
-   Callback = function()
-      game:GetService("ReplicatedStorage"):WaitForChild("CutInLineEvent2"):FireServer(unpack(args))
-   end,
- })
- 
- local Push = Tab:CreateButton({
-   Name = "Push",
-   Callback = function()
-      game:GetService("ReplicatedStorage"):WaitForChild("PUSHPLAYERTHATISFRONT"):FireServer(unpack(args))
-   end,
- })
- 
- local Push = Tab:CreateButton({
-   Name = "Add 1T Money",
-   Callback = function()
-      addMoney(1000000000000)
-   end,
- })
+
+   game:GetService("ReplicatedStorage"):WaitForChild("CutInLineEvent2"):FireServer(unpack(args2))
+
+   local args3 = {
+     [1] = "CLUBCLUBGOODLUCK777888BESTGAMEPLAY99COOLGAMECHANGEDO"
+  }
+  game:GetService("ReplicatedStorage"):WaitForChild("PUSHMEBACK"):FireServer(unpack(args3))
+end
+
+local Window = Rayfield:CreateWindow({
+  Name = "Scrivts",
+  LoadingTitle = "Freaky Prison Line Simulator",
+  LoadingSubtitle = "by Scrivts",
+
+  KeySystem = false,
+
+   KeySettings = {
+     Title = "Scrivts",
+     Subtitle = "Key System",
+     Note = "The key is Scrivts",
+     Key = {"Scrivts"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+  }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+
+local Cut = Tab:CreateButton({
+  Name = "Cut in line",
+  Callback = function()
+     game:GetService("ReplicatedStorage"):WaitForChild("CutInLineEvent2"):FireServer(unpack(args))
+  end,
+})
+
+local Push = Tab:CreateButton({
+  Name = "Push",
+  Callback = function()
+     game:GetService("ReplicatedStorage"):WaitForChild("PUSHPLAYERTHATISFRONT"):FireServer(unpack(args))
+  end,
+})
+
+local Push = Tab:CreateButton({
+  Name = "Add 1T Money",
+  Callback = function()
+     addMoney(1000000000000)
+  end,
+})
