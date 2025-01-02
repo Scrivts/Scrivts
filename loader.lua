af570a32d619f1ce2eb740efce942ff26ba1162b
ofatrii <149586012+ofatrii@users.noreply.github.com>
2025-01-02 19:13:46 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,6 @@
 Scrivts Loader
 --]]
 
---loadstring(game:HttpGet("https://raw.githubusercontent.com/scrivts/Scrivts/refs/heads/main/79974059203827/source.lua"))()
 local success = pcall(function()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/scrivts/Scrivts/refs/heads/main/"..game.PlaceId.."/source.lua"))()
 end)
